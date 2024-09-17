f2f6f1d95366a565dfd29ea1c932f79b46203dac
Yytsi <tuukka.yildirim@gmail.com>
2024-09-16 20:12:11 -0400

    10.4

---
+++ rate-repository-app/src/components/AppBar.jsx
@@ -0,0 +1,18 @@
+import { View, StyleSheet } from "react-native";
+import Constants from "expo-constants";
+
+import theme from "../theme";
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: theme.appBar.backgroundColor,
+    height: theme.appBar.height,
+  },
+});
+
+const AppBar = ({ children }) => {
+  return <View style={styles.container}>{children}</View>;
+};
+
+export default AppBar;
